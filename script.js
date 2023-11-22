60d37bcd5d1eaed6939ab493193d3fd292c0be5d
GustavoZad <109832524+GustavoZad@users.noreply.github.com>
2023-11-22 13:23:02 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+let celsiusInput = document.querySelector('#celsiusInput');
+let fahrenheitInput = document.querySelector('#fahrenheitInput');
+let kelvinInput = document.querySelector('#kelvinInput');
+let clearButton = document.querySelector('#clearButton');
+
+function roundNumber(number) {
+    return Math.round(number * 100) / 100;
+}
+
+function convertTemperatures() {
+    let cTemp = parseFloat(celsiusInput.value);
+    let fTemp = (cTemp * 9/5) + 32;
+    let kTemp = cTemp + 273.15;
+
+    fahrenheitInput.value = roundNumber(fTemp);
+    kelvinInput.value = roundNumber(kTemp);
+}
+
+celsiusInput.addEventListener('input', convertTemperatures);
+fahrenheitInput.addEventListener('input', function(){
+    let fTemp = parseFloat(fahrenheitInput.value);
+    let cTemp = (fTemp - 32) * 5/9;
+    let kTemp = (fTemp - 32) * 5/9 + 273.15;
+
+    celsiusInput.value = roundNumber(cTemp);
+    kelvinInput.value = roundNumber(fTemp);
+});
+kelvinInput.addEventListener('input', function () {
+    let kTemp = parseFloat(kelvinInput.value);
+    let cTemp = kTemp - 273.15;
+    let fTemp = (kTemp - 273.15) * 9/5 + 32;
+
+    celsiusInput.value = roundNumber(cTemp);
+    fahrenheitInput.value = roundNumber(fTemp);
+});
+
+clearButton.addEventListener('click', function() {
+    celsiusInput.value = "";
+    fahrenheitInput.value = "";
+    kelvinInput.value = "";
+})
